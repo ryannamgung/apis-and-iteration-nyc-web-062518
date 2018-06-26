621868047dd0b227795561a513adeaa3d0c43c98
ryannamgung <ryannamgung@gmail.com>
2018-06-26 17:39:15 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,22 +4,53 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  count = 1
+  loop do
+    # binding.pry
+    all_characters = RestClient.get("https://swapi.co/api/people/?page=#{count}")
+    character_hash = JSON.parse(all_characters)
+    # binding.pry
+
+    # iterate over the character hash to find the collection of `films` for the given
+    #   `character`
+    # collect those film API urls, make a web request to each URL to get the info
+    #  for that film
+    # return value of this method should be collection of info about each film.
+    #  i.e. an array of hashes in which each hash reps a given film
+    # this collection will be the argument given to `parse_character_movies`
+    #  and that method will do some nice presentation stuff: puts out a list
+    #  of movies by title. play around with puts out other info about a given film.
+    # binding.pry
+    use_hash = character_hash["results"].find do |hash|
+      hash["name"].downcase == character
+      # binding.pry
+      # binding.pry
+    end
+    if use_hash
+      collection = []
+      use_hash["films"].each do |urls|
+        movie_names = convert(urls)
+        collection << movie_names
+        # binding.pry
+      end
+      return collection
+    elsif count > 9
+      break
+    end
+    count += 1
+  end
+end
+
+def convert(urls)
+  url_link = RestClient.get(urls)
+  JSON.parse(url_link)
 end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts film["title"]
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome!"
 end
 
 def get_character_from_user
   puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  user_input = gets.chomp.downcase
+  user_input
 end
